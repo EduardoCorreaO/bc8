96476abcf01face9fa9184dc8a6d18acc5230560
Eduardo Enrique Correa Ordenes <eduardo.correa@tsoftglobal.com>
2022-05-09 20:22:54 -0400

    apitesting

---
+++ src/test/java/api/Ecorrea/MiPrimerTestConRestAssured.java
@@ -0,0 +1,18 @@
+package api.Ecorrea;
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.json.simple.JSONObject;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class MiPrimerTestConRestAssured {
+
+
+
+
+}
